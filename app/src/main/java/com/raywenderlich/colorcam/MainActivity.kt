4f70a7775e471965ca814acc781843a66c7810c5
James Edwards <jedwards@randmcnally.com>
2020-01-02 09:24:53 -0600

    Work with a TextureView

--- app/src/main/java/com/raywenderlich/colorcam/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/colorcam/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.raywenderlich.colorcam
 
 import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.view.Surface
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraX
 import androidx.core.app.ActivityCompat
@@ -59,6 +61,25 @@ class MainActivity : AppCompatActivity() {
         finish()
       }
     }
+  }
+
+  private fun updateTextureView(){
+
+    val centerY = textureView.height / 2f
+    val centerX = textureView.width / 2f
+    val matrix = Matrix()
+
+    val rotationDegree = when(textureView.display.rotation){
+
+      Surface.ROTATION_0 -> 0
+      Surface.ROTATION_90 -> 90
+      Surface.ROTATION_180 -> 180
+      Surface.ROTATION_270 -> 270
+      else -> return
+    }
+
+    matrix.postRotate(-rotationDegree.toFloat(), centerX,centerY)
+    textureView.setTransform(matrix)
   }
 }
 
